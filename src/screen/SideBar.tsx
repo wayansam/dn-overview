6924b29d0a5a7ba7481148133c8901db8dde19e4
wayansam <42888175+wayansam@users.noreply.github.com>
2025-03-28 07:25:36 +0000

    revamp side bar

--- src/screen/SideBar.tsx
+++ src/screen/SideBar.tsx
@@ -1,23 +1,38 @@
-import { Button, Divider, Layout, Space, theme } from "antd";
+import {
+  Button,
+  Divider,
+  Layout,
+  Space,
+  theme,
+  Grid,
+  Card,
+} from "antd";
 import { useState } from "react";
 import { TAB_GROUP_LIST } from "../constants/Common.constants";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { setSelectedSideBar } from "../slice/UIState.reducer";
 
 const { Sider } = Layout;
+const { useBreakpoint } = Grid;
 
 const SideBar = () => {
   const dispatch = useAppDispatch();
 
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+  const { token } = theme.useToken();
+  const screens = useBreakpoint();
 
   const selectedSideBar = useAppSelector(
     (state) => state.UIState.selectedSideBar
   );
 
   const [isSmall, setIsSmall] = useState(false);
+
+  const getWidthSetting = () => {
+    if (screens.xs) {
+      return 210;
+    }
+    return 250;
+  };
 
   return (
     <Sider
@@ -38,6 +53,7 @@ const SideBar = () => {
             }
           : undefined
       }
+      width={getWidthSetting()}
     >
       <div
         style={{
@@ -46,33 +62,48 @@ const SideBar = () => {
         }}
       >
         <Space
+          size={"small"}
           direction="vertical"
           style={{
             width: "100%",
-            padding: "10px",
+            padding: "10px 15px 30px 0px",
+            marginBottom: 30,
             borderWidth: 1,
+            maxWidth: 200,
+            borderBottomRightRadius: token.borderRadiusLG,
+            backgroundColor: token.colorBgContainer,
           }}
         >
           {TAB_GROUP_LIST.map((group) => (
-            <>
-              <Divider
-                style={{ color: "white", margin: 0, borderBlockStart: "white" }}
-                orientation={"left"}
-              >
+            <Card
+              size="small"
+              key={`card-${group.name}`}
+              bordered={false}
+              style={{ paddingLeft: 0 }}
+              bodyStyle={{ paddingLeft: 0 }}
+            >
+              <Divider style={{ margin: 0 }} orientation={"left"}>
                 {group.name}
               </Divider>
               {group.children.map((item) => (
                 <Button
                   block
+                  style={{
+                    border: "none",
+                    borderTopLeftRadius: 0,
+                    borderBottomLeftRadius: 0,
+                    textAlign: "right",
+                  }}
                   type={
                     selectedSideBar.key === item.key ? "primary" : "default"
                   }
                   onClick={() => dispatch(setSelectedSideBar(item))}
+                  size={"middle"}
                 >
                   {item.name}
                 </Button>
               ))}
-            </>
+            </Card>
           ))}
         </Space>
       </div>
